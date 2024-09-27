625f9699916d903ec708546e53d834b4ecc81624
Ari Mahpour <Ari.Mahpour@gmail.com>
2024-09-26 20:24:49 -0700

    Updated shell script and instructions.

--- kv260/build_demo.sh
+++ kv260/build_demo.sh
@@ -6,11 +6,9 @@ git clean -xfd
 # Make the platform
 make platform PFM=kv260_ispMipiRx_vcu_DP
 
-# Make the IP
+# Make the IP in the appropriate directory
 pushd platforms/vivado/kv260_ispMipiRx_rpiMipiRx_DP/ip/isp_single_kv260/
 make ip
-
-# Move back to kv260/ folder
 popd
 
 # Copy the single_isp IP over
@@ -20,3 +18,13 @@ cp -rf platforms/vivado/kv260_ispMipiRx_rpiMipiRx_DP/ip/isp_single_kv260/ platfo
 cp extras/pin.xdc platforms/vivado/kv260_ispMipiRx_vcu_DP/xdc/pin.xdc
 cp extras/config_bd.tcl platforms/vivado/kv260_ispMipiRx_vcu_DP/scripts/config_bd.tcl
 cp extras/prj_config_1dpu overlays/examples/smartcam/prj_conf/prj_config_1dpu
+
+# Build the demo
+make clean
+make overlay OVERLAY=smartcam
+
+# Convert system.bit to kv260-raspi-dpu.bit.bin in the appropriate directory
+pushd overlays/examples/smartcam/binary_container_1/link/int/
+echo 'all:{system.bit}' > bootgen.bif
+bootgen -w -arch zynqmp -process_bitstream bin -image bootgen.bif
+mv system.bit.bin kv260-raspi-dpu.bit.bin
--- kv260/rpi-camera-demo.md
+++ kv260/rpi-camera-demo.md
@@ -5,6 +5,7 @@ This is a simplified tutorial of [adding Raspberry Pi Camera to SmartCam’s Pla
 * Vivado 2022.1 installed on Linux (full installation)
 * Kria KV260 Vision AI Starter Kit
 * Raspberry Pi Camera (v2)
+* Ubuntu 22.04 with Docker installed on the Kria SOM
 
 ## Cloning the Repository
 You'll need to clone the repository and check out the `xlnx_rel_v2022.1` branch using the following command:
@@ -12,16 +13,8 @@ You'll need to clone the repository and check out the `xlnx_rel_v2022.1` branch 
 git clone --recursive --branch xlnx_rel_v2022.1 https://github.com/Xilinx/kria-vitis-platforms.git
 ```
 
-## Making the platform and IP
-Make the platform
+## Building the Project
 ```
-cd kria-vitis-platforms/kv260/
-make platform PFM=kv260_ispMipiRx_vcu_DP
-```
-
-Make the IP
-```
-cd platforms/vivado/kv260_ispMipiRx_rpiMipiRx_DP/ip/isp_single_kv260/
-```
-make ip
+cd kv260/
+./build_demo.sh
 ```
